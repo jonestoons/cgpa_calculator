e53fccf8918929e7209e6e233fdb2f12bb757880
jonestoons <tee.barisi@gmail.com>
2020-11-10 14:33:05 +0100

    preliminary setup

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: child,
+      child: Container(
+        child: Center(
+          child: Text('Yay CGPA Calc coming up...'),
+        ),
+      ),
     );
   }
 }
